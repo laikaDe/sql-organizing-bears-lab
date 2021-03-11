b8dd9de1124a9ee17aa99535ecf6224ec656fcb6
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-03-10 21:36:17 -0800

    commit tree 8741da9659ea314efb430b3cd69d796a182cb815

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 25, 'F', 'pink', 'stupid', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'purple', 'sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 40, 'M', 'green', 'rude', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Seargant Brown', 12, 'F', 'purple', 'snooty', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 36, 'M', 'navy', 'smart', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 30, 'F', 'red', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'M', 'orange', 'stupid', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, 'F', 'rainbow', 'petty', 1);
